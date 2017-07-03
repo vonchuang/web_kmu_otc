978e110c10b311b4dea32de7b1ec9dc0412df0be
vonchuang <chuang8673@gmail.com>
2017-07-03 15:17:31 +0800

    modify infor js

--- js/script_infor.js
+++ js/script_infor.js
@@ -1,6 +1,204 @@
 // JavaScript source code
 $(document).ready(function () {
-    
+
+    if (location.hash == '#article-1') {
+        $('.article-1').show();
+        $('.article-2').hide();
+        $('.article-3').hide();
+        $('.article-4').hide();
+        $('.article-5').hide();
+        $('.article-6').hide();
+        $('.article-7').hide();
+        $('.article-8').hide();
+        $('.article-9').hide();
+        $('.article-10').hide();
+        $('.article-11').hide();
+        $('.article-12').hide();
+        $('.article-13').hide();
+
+    } else if (location.hash == '#article-2') {
+        $('.article-1').hide();
+        $('.article-2').show();
+        $('.article-3').hide();
+        $('.article-4').hide();
+        $('.article-5').hide();
+        $('.article-6').hide();
+        $('.article-7').hide();
+        $('.article-8').hide();
+        $('.article-9').hide();
+        $('.article-10').hide();
+        $('.article-11').hide();
+        $('.article-12').hide();
+        $('.article-13').hide();
+
+    } else if (location.hash == '#article-3') {
+        $('.article-1').hide();
+        $('.article-2').hide();
+        $('.article-3').show();
+        $('.article-4').hide();
+        $('.article-5').hide();
+        $('.article-6').hide();
+        $('.article-7').hide();
+        $('.article-8').hide();
+        $('.article-9').hide();
+        $('.article-10').hide();
+        $('.article-11').hide();
+        $('.article-12').hide();
+        $('.article-13').hide();
+
+    } else if (location.hash == '#article-4') {
+        $('.article-1').hide();
+        $('.article-2').hide();
+        $('.article-3').hide();
+        $('.article-4').show();
+        $('.article-5').hide();
+        $('.article-6').hide();
+        $('.article-7').hide();
+        $('.article-8').hide();
+        $('.article-9').hide();
+        $('.article-10').hide();
+        $('.article-11').hide();
+        $('.article-12').hide();
+        $('.article-13').hide();
+
+    } else if (location.hash == '#article-5') {
+        $('.article-1').hide();
+        $('.article-2').hide();
+        $('.article-3').hide();
+        $('.article-4').hide();
+        $('.article-5').show();
+        $('.article-6').hide();
+        $('.article-7').hide();
+        $('.article-8').hide();
+        $('.article-9').hide();
+        $('.article-10').hide();
+        $('.article-11').hide();
+        $('.article-12').hide();
+        $('.article-13').hide();
+
+    } else if (location.hash == '#article-6') {
+        $('.article-1').hide();
+        $('.article-2').hide();
+        $('.article-3').hide();
+        $('.article-4').hide();
+        $('.article-5').hide();
+        $('.article-6').show();
+        $('.article-7').hide();
+        $('.article-8').hide();
+        $('.article-9').hide();
+        $('.article-10').hide();
+        $('.article-11').hide();
+        $('.article-12').hide();
+        $('.article-13').hide();
+
+    } else if (location.hash == '#article-7') {
+        $('.article-1').hide();
+        $('.article-2').hide();
+        $('.article-3').hide();
+        $('.article-4').hide();
+        $('.article-5').hide();
+        $('.article-6').hide();
+        $('.article-7').show();
+        $('.article-8').hide();
+        $('.article-9').hide();
+        $('.article-10').hide();
+        $('.article-11').hide();
+        $('.article-12').hide();
+        $('.article-13').hide();
+
+    } else if (location.hash == '#article-8') {
+        $('.article-1').hide();
+        $('.article-2').hide();
+        $('.article-3').hide();
+        $('.article-4').hide();
+        $('.article-5').hide();
+        $('.article-6').hide();
+        $('.article-7').hide();
+        $('.article-8').show();
+        $('.article-9').hide();
+        $('.article-10').hide();
+        $('.article-11').hide();
+        $('.article-12').hide();
+        $('.article-13').hide();
+
+    } else if (location.hash == '#article-9') {
+        $('.article-1').hide();
+        $('.article-2').hide();
+        $('.article-3').hide();
+        $('.article-4').hide();
+        $('.article-5').hide();
+        $('.article-6').hide();
+        $('.article-7').hide();
+        $('.article-8').hide();
+        $('.article-9').show();
+        $('.article-10').hide();
+        $('.article-11').hide();
+        $('.article-12').hide();
+        $('.article-13').hide();
+
+    } else if (location.hash == '#article-10') {
+        $('.article-1').hide();
+        $('.article-2').hide();
+        $('.article-3').hide();
+        $('.article-4').hide();
+        $('.article-5').hide();
+        $('.article-6').hide();
+        $('.article-7').hide();
+        $('.article-8').hide();
+        $('.article-9').hide();
+        $('.article-10').show();
+        $('.article-11').hide();
+        $('.article-12').hide();
+        $('.article-13').hide();
+
+    } else if (location.hash == '#article-11') {
+        $('.article-1').hide();
+        $('.article-2').hide();
+        $('.article-3').hide();
+        $('.article-4').hide();
+        $('.article-5').hide();
+        $('.article-6').hide();
+        $('.article-7').hide();
+        $('.article-8').hide();
+        $('.article-9').hide();
+        $('.article-10').hide();
+        $('.article-11').show();
+        $('.article-12').hide();
+        $('.article-13').hide();
+
+    } else if (location.hash == '#article-12') {
+        $('.article-1').hide();
+        $('.article-2').hide();
+        $('.article-3').hide();
+        $('.article-4').hide();
+        $('.article-5').hide();
+        $('.article-6').hide();
+        $('.article-7').hide();
+        $('.article-8').hide();
+        $('.article-9').hide();
+        $('.article-10').hide();
+        $('.article-11').hide();
+        $('.article-12').show();
+        $('.article-13').hide();
+
+    } else if (location.hash == '#article-13') {
+        $('.article-1').hide();
+        $('.article-2').hide();
+        $('.article-3').hide();
+        $('.article-4').hide();
+        $('.article-5').hide();
+        $('.article-6').hide();
+        $('.article-7').hide();
+        $('.article-8').hide();
+        $('.article-9').hide();
+        $('.article-10').hide();
+        $('.article-11').hide();
+        $('.article-12').hide();
+        $('.article-13').show();
+
+    }
+
+    /*
     $('.second-nav-1').click(function () {
         $('.article-1').show();
         $('.article-2').hide();
@@ -208,4 +406,5 @@ $(document).ready(function () {
         $('.article-12').hide();
         $('.article-13').show();
     });
+    */
 });
