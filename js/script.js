44a36782011c9797183ad4a6c3fc8e49285820ac
vonchuang <chuang8673@gmail.com>
2017-07-05 18:25:23 +0800

    change color

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 $(document).ready(function () {
 
     $('#nav_header').navPlugin({
-        'itemWidth': 160,
-        'itemHeight': 30,
+        'itemWidth': 180,
+        'itemHeight': 71,
         'navEffect': "slide",
         'speed': 250
     });
